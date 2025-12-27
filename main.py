f2a4e29e1aa7d67cf8eb162b2258131ead211df4
jianshujie <yaoqiwood@gmail.com>
2025-12-27 23:22:13 +0800

    feat(API): 添加新的items相关API端点
    新增多个items相关API端点，包括分页查询、带参数查询等功能

--- main.py
+++ main.py
@@ -11,9 +11,35 @@ async def root():
     return {"Hello": "World"}
 
 
+@app.get("/items/")
+async def read_items(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
+
+
 @app.get("/items/{item_id}")
 async def read_item(item_id: int):
     return {"item_id": item_id}
+
+
+@app.get("/items/readitem/{item_id}")
+async def read_item_new(item_id: str, q: str | None = None):
+    if q:
+        return {"item_id": item_id, "q": q}
+    return {"item_id": item_id}
+
+
+@app.get("/items/read_item_new/{item_id}")
+async def read_item_another(item_id: str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id}
+    print(q)
+    if q:
+        item.update({"q": q})
+        print(item)
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
 
 
 @app.get("/models/{model_name}")
@@ -28,6 +54,9 @@ async def get_model(model_name: ModelName):
 @app.get("/files/{file_path:path}")
 async def read_file(file_path: str):
     return {"file_path": file_path}
+
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
 
 if __name__ == "__main__":
